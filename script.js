6db69b5ce2de0c93f48275e1f5d2edf4a30d9e4e
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2024-08-06 21:20:27 +0200

    update css

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const header = document.getElementById("header");
-const title = document.getElementById("title");
-const short = document.getElementById("short");
-const profile = document.getElementById("profile-img");
-const author_name = document.getElementById("name");
-const date = document.getElementById("date");
+document.addEventListener("DOMContentLoaded", function () {
+  const headers = document.querySelectorAll(".header");
+  const titles = document.querySelectorAll(".title");
+  const shorts = document.querySelectorAll(".short");
+  const profiles = document.querySelectorAll(".profile-img");
+  const author_names = document.querySelectorAll(".name");
+  const dates = document.querySelectorAll(".date");
 
-const animated_bgs = document.querySelectorAll(".animated-bg");
-const animated_bg_text = document.querySelectorAll(".animated-bg-text");
+  const animated_bgs = document.querySelectorAll(".animated-bg");
+  const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
 
-setTimeout(getData, 1000);
+  setTimeout(getData, 1000);
 
-function getData() {
-  header.innerHTML = '<img src="game.png" />';
-  title.innerHTML = "Lorem ipsum dolor sit amet.";
-  short.innerHTML =
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam! " +
-    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum? ";
-  profile.innerHTML = '<img src="greenish2.png" alt="" />';
-  author_name.innerHTML = "Mr Greenish";
-  date.innerHTML = "Today";
-  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
-  animated_bg_text.forEach((bg) => bg.classList.remove("animated-bg-text"));
-}
+  function getData() {
+    headers.forEach(
+      (header) => (header.innerHTML = '<img src="game.png" alt="Game Image" />')
+    );
+    titles.forEach(
+      (title) => (title.innerHTML = "Lorem ipsum dolor sit amet.")
+    );
+    shorts.forEach(
+      (short) =>
+        (short.innerHTML =
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum?")
+    );
+    profiles.forEach(
+      (profile) =>
+        (profile.innerHTML = '<img src="greenish2.png" alt="Profile Image" />')
+    );
+    author_names.forEach(
+      (author_name) => (author_name.innerHTML = "Mr Greenish")
+    );
+    dates.forEach((date) => (date.innerHTML = "Today"));
+
+    animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+    animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+  }
+});
